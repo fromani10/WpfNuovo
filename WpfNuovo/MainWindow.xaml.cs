7653292edc46f338f74e7b394bbc0e9e04c4bdaf
fromani10 <romanifede10@gmail.com>
2023-11-15 12:16:57 +0100

    dd

--- WpfNuovo/MainWindow.xaml.cs
+++ WpfNuovo/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace WpfNuovo
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
